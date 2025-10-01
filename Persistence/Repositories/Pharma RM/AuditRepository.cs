3460a24dafe076d9e09fc7b9cb6a59b5a1dbf12c
kunalwadibharme-sdn <kunalwadibharme@smartdatainc.net>
2025-10-01 12:02:06 +0530

    Updated Sp calling by Dapper

--- Persistence/Repositories/Pharma RM/AuditRepository.cs
+++ Persistence/Repositories/Pharma RM/AuditRepository.cs
@@ -4,9 +4,12 @@ using Microsoft.EntityFrameworkCore;
 using Services.IRepositories.Pharma_RM;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
+
 
 namespace Persistence.Repositories.Pharma_RM
 {
@@ -20,22 +23,49 @@ namespace Persistence.Repositories.Pharma_RM
         }
 
 
+        //public async Task<PagedAuditDetailDto> GetAllAsync(AuditRequestDto auditRequestDto)
+        //{
+        //    var auditRecords = await _context.AuditDetailDto
+        //        .FromSqlInterpolated($@"EXEC dbo.GetAuditDetails 
+        //                        @StatusId = {auditRequestDto.StatusId}, 
+        //                        @SearchText = {auditRequestDto.SearchText},                                                                                    
+        //                        @PageNumber = {auditRequestDto.PageNumber}, 
+        //                        @PageSize = {auditRequestDto.PageSize}")
+        //        .AsNoTracking()
+        //        .ToListAsync();
+
+        //    var totalCount = auditRecords.FirstOrDefault()?.TotalCount ?? 0;
+
+        //    return new PagedAuditDetailDto
+        //    {
+        //        Records = auditRecords.ToArray(),
+        //        PageNumber = auditRequestDto.PageNumber,
+        //        PageSize = auditRequestDto.PageSize,
+        //        TotalCount = totalCount
+        //    };
+        //}
         public async Task<PagedAuditDetailDto> GetAllAsync(AuditRequestDto auditRequestDto)
         {
-            var auditRecords = await _context.AuditDetailDto
-                .FromSqlInterpolated($@"EXEC dbo.GetAuditDetails 
-                                @StatusId = {auditRequestDto.StatusId}, 
-                                @SearchText = {auditRequestDto.SearchText},                                                                                    
-                                @PageNumber = {auditRequestDto.PageNumber}, 
-                                @PageSize = {auditRequestDto.PageSize}")
-                .AsNoTracking()
-                .ToListAsync();
+            using var connection = _context.Database.GetDbConnection();
 
-            var totalCount = auditRecords.FirstOrDefault()?.TotalCount ?? 0;
+            var auditRecords = await connection.QueryAsync<AuditDetailDto>(
+                "dbo.GetAuditDetails",
+                new
+                {
+                    StatusId = auditRequestDto.StatusId,
+                    SearchText = auditRequestDto.SearchText,
+                    PageNumber = auditRequestDto.PageNumber,
+                    PageSize = auditRequestDto.PageSize
+                },
+                commandType: CommandType.StoredProcedure
+            );
+
+            var recordsList = auditRecords.ToList();
+            var totalCount = recordsList.FirstOrDefault()?.TotalCount ?? 0;
 
             return new PagedAuditDetailDto
             {
-                Records = auditRecords.ToArray(),
+                Records = recordsList.ToArray(),
                 PageNumber = auditRequestDto.PageNumber,
                 PageSize = auditRequestDto.PageSize,
                 TotalCount = totalCount
