6673c685d1013b4b1cd56946a43c33087089087d
harshitamirase01 <harshitamarotimirase@smartdatainc.net>
2025-10-01 10:44:12 +0530

    migration

---
+++ Persistence/Migrations/20250930093240_AddMissingChanges.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditDetailDto",
+                columns: table => new
+                {
+                    AuditId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SupplierId = table.Column<int>(type: "int", nullable: false),
+                    AuditorName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuditTypeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Score = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    AuditDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditDetailDto");
+        }
+    }
+}
